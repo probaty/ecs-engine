1b11d0e37a96846b9daab163ea02558766ee8b65
mshuktuev <travokur321@yandex.ru>
2024-03-05 17:38:11 +0500

    update component for new entity

--- packages/ecs/core/Component.ts
+++ packages/ecs/core/Component.ts
@@ -1,8 +1,16 @@
+import type { Container } from "pixi.js";
+
 export class Component {
-	public id: string;
-	constructor() {
-		this.id = crypto.randomUUID();
-	}
+  protected _pixiContainer: Container | null = null
+  public id: string
+
+  constructor() {
+    this.id = crypto.randomUUID()
+  }
+
+  public set pixiContainer(cont: Container) {
+    this._pixiContainer = cont;
+  }
 }
 
 export type ComponentConstructor = new (...args: any[]) => Component;
