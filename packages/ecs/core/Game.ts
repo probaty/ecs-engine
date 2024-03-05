3f4ec3c79f9ec554268b0a0cc5530802b1127c99
mshuktuev <travokur321@yandex.ru>
2024-03-05 17:36:31 +0500

    add game run logic

--- packages/ecs/core/Game.ts
+++ packages/ecs/core/Game.ts
@@ -1,48 +1,89 @@
 import { Application, type IApplicationOptions } from "pixi.js";
 import type { Addon } from "./Addon";
+import { Scene } from "./Scene";
+import Stats from "stats.js";
 
 type Options = {};
 export type GameOptions = Partial<Options> & Partial<IApplicationOptions>;
 export type BasicGameState = {
-	view: {
-		width: number;
-		height: number;
-	};
-	game: Game;
+  view: {
+    width: number;
+    height: number;
+  };
+  game: Game;
+  tickers: Set<(dt: number) => void>
 } & Record<string, unknown>;
 
 const defaultOptions: GameOptions = {
-	resizeTo: window,
-	antialias: true,
-	eventMode: "static",
+  resizeTo: window,
+  antialias: true,
+  eventMode: "static",
 };
 
 export class Game extends Application {
-	public gameState: BasicGameState;
+  public gameState: BasicGameState;
 
-	private _addons = new Set<Addon<unknown>>();
+  private _addons = new Set<Addon<unknown>>();
+  private _scenes = new Map<string, Scene>()
+  private _currentScene: Scene | null = null
+  private _stats: Stats
 
-	constructor(options?: GameOptions) {
-		options = { ...defaultOptions, ...options };
-		super(options);
+  constructor(options?: GameOptions) {
+    options = { ...defaultOptions, ...options };
+    super(options);
+    this._stats = new Stats()
+    this._stats.showPanel(1)
+    document.body.appendChild(this._stats.dom)
 
-		document.appendChild(this.view as HTMLCanvasElement);
-		this.gameState = {
-			view: {
-				width: this.view.width,
-				height: this.view.height,
-			},
-			game: this,
-		};
-	}
+    document.body.appendChild(this.view as HTMLCanvasElement);
+    this.gameState = {
+      view: {
+        width: this.view.width,
+        height: this.view.height,
+      },
+      game: this,
+      tickers: new Set()
+    };
+  }
 
-	/**
-	 * registerAddons
-	 */
-	public registerAddons(addons: Addon<unknown>[]) {
-		for (const addon of addons) {
-			this.gameState[addon.name] = {};
-			this._addons.add(addon);
-		}
-	}
+  /**
+   * run
+   */
+  public async run() {
+    console.log(this.gameState);
+    await Promise.all([...(this._addons.values())].map(val => val.loading))
+    this._addons.forEach(addon => {
+      addon.onCreate(this.gameState)
+    })
+    if (this._currentScene) {
+      this.stage.addChild(this._currentScene.scene)
+      this.ticker = this._currentScene.run(this.gameState)
+      this.ticker.add(this._stats.begin)
+      this.gameState.tickers.forEach(t => {
+        this.ticker.add(t)
+      })
+      this.ticker.add(this._stats.end)
+      this.ticker.start()
+
+
+    }
+  }
+
+  /**
+   * addScene
+   */
+  public addScene(scene: Scene) {
+    this._scenes.set(scene.name, scene)
+    this._currentScene = scene
+  }
+
+  /**
+   * registerAddons
+   */
+  public registerAddons(addons: Addon<unknown>[]) {
+    for (const addon of addons) {
+      this.gameState[addon.name] = {};
+      this._addons.add(addon);
+    }
+  }
 }
