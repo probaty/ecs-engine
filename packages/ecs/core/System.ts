fe9340ff5caf890276298d19948c012e7caa7779
mshuktuev <travokur321@yandex.ru>
2024-03-05 17:39:57 +0500

    refactor system and fix types

--- packages/ecs/core/System.ts
+++ packages/ecs/core/System.ts
@@ -1,37 +1,42 @@
-import type { Component } from "./Component";
+import type { Component, ComponentConstructor } from "./Component";
+import type { BasicGameState } from "./Game";
 
 export type Query<T> = T[];
 
+type CombineGS<T extends Record<string, any>> = BasicGameState & T
+
 export interface System<T = Component | Component[], GS = any> {
-	query: T;
-	update?: (gameState: GS, query: Query<T>, delta?: number) => void;
-	onCreate?: (gameState: GS, query: Query<T>) => void;
-	onDestroy?: (gameState: GS, query: Query<T>) => void;
+  query: ComponentConstructor | ComponentConstructor[];
+  update?: (gameState: GS, query: Query<T>, delta: number) => void;
+  onCreate?: (gameState: GS, query: Query<T>) => void;
+  onDestroy?: (gameState: GS, query: Query<T>) => void;
 }
 
 export type updateFunction<T = Component | Component[], GS = any> = (
-	gameState: GS,
-	query: Query<T>,
+  gameState: GS,
+  query: Query<T>,
 ) => void;
 
 export type onCreateFunction<T = Component | Component[], GS = any> = (
-	gameState: GS,
-	query: Query<T>,
+  gameState: GS,
+  query: Query<T>,
 ) => void;
 
 export type onDestroyFunction<T = Component | Component[], GS = any> = (
-	gameState: GS,
-	query: Query<T>,
+  gameState: GS,
+  query: Query<T>,
 ) => void;
 
 export function createSystem<
-	const T extends Component[] | Component = Component | Component[],
-	GS = any,
->(query: T, { onCreate, onDestroy, update }: Omit<System<T, GS>, "query"> = {}): System<T, GS> {
-	return {
-		query,
-		update,
-		onCreate,
-		onDestroy,
-	};
+  const T extends Component[] | Component,
+  GS extends Record<string, any> = {},
+>(query: ComponentConstructor | ComponentConstructor[], { onCreate, onDestroy, update }: Omit<System<T, CombineGS<GS>>, "query"> = {}): System<T, CombineGS<GS>> {
+
+  return {
+    query,
+    update,
+    onCreate,
+    onDestroy,
+  };
 }
+
