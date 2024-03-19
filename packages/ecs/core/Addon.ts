285639ecdfdb8f32f837c1b4d79c3e3227ff407c
mshuktuev <travokur321@yandex.ru>
2024-03-19 22:05:26 +0500

    fix: clean addon

--- packages/ecs/core/Addon.ts
+++ packages/ecs/core/Addon.ts
@@ -1,9 +1,8 @@
-import type { Application } from "pixi.js";
 import type { BasicGameState } from "./Game";
 
 export abstract class Addon<GS = BasicGameState> {
-  public loading = new Promise<void>((res) => res())
-  public abstract name: string;
-  public abstract onCreate(gameState: GS): void;
-  public abstract onDestroy(gameState: GS): void;
+	public loading = new Promise<void>((res) => res());
+	public abstract name: string;
+	public abstract onCreate(gameState: GS): void;
+	public abstract onDestroy(gameState: GS): void;
 }
